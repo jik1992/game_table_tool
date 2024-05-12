29a69f72e64a6be9ce6d539356b370ba59fae3d9
yunzuo <yunzuo@accutarbio.com>
2024-05-12 18:35:12 +0800

    11

--- src/component/MapResource.tsx
+++ src/component/MapResource.tsx
@@ -8,7 +8,7 @@ import 'handsontable/dist/handsontable.full.min.css';
 
 import {registerAllModules} from 'handsontable/registry';
 import {registerLanguageDictionary, zhCN} from 'handsontable/i18n';
-import {Button, Select} from "antd";
+import {Button, Input, Select} from "antd";
 import axios from "axios";
 import {HotTable} from "@handsontable/react";
 import HotTableClass from "@handsontable/react/hotTableClass";
@@ -20,6 +20,7 @@ registerAllModules();
 type IProps = {};
 
 type IState = {
+    destination: string,
     rows: any[][]
 };
 
@@ -27,7 +28,8 @@ type IState = {
 export class MapResource extends React.PureComponent<IProps, IState> {
 
     state: IState = {
-        rows: []
+        rows: [],
+        destination: ''
     };
 
     componentDidMount() {
@@ -41,7 +43,7 @@ export class MapResource extends React.PureComponent<IProps, IState> {
     private exportToCsv = () => {
         if (this.hotTableRef.current) {
             const hot = this.hotTableRef.current.hotInstance;
-            if (hot){
+            if (hot) {
                 const exportPlugin = hot.getPlugin('exportFile');
                 const blob = exportPlugin.exportAsBlob('csv')
                 const link = document.createElement('a');
@@ -59,6 +61,32 @@ export class MapResource extends React.PureComponent<IProps, IState> {
         }
     };
 
+    private save = () => {
+        if (this.hotTableRef.current) {
+            const hot = this.hotTableRef.current.hotInstance;
+            if (hot) {
+                const exportPlugin = hot.getPlugin('exportFile');
+                const csv = exportPlugin.exportAsString('csv')
+                for (const row of csv.split("\n")) {
+                    const co = row.split(",")
+                    const x = Number.parseInt(co[5])
+                    const y = Number.parseInt(co[6].replaceAll("\"", ''))
+                    const owner = co[7].replaceAll("\r", '')
+                    if (!_.isEmpty(owner) && owner !== '\r') {
+                        console.info(x, y, owner)
+                    }
+                }
+            }
+        }
+    };
+
+    private calculateDistance(point1: { x: number, y: number }, point2: { x: number, y: number }) {
+        const dx = point1.x - point2.x;
+        const dy = point1.y - point2.y;
+        const distance = Math.hypot(dx, dy);
+        return _.round(distance);
+    }
+
     render() {
         const {rows} = this.state
         return <>
@@ -69,15 +97,15 @@ export class MapResource extends React.PureComponent<IProps, IState> {
                 }}
                 options={[
                     {
-                    value: '官渡之战',
-                    label: `官渡之战`,
-                }, {
-                    value: '奇门八阵',
-                    label: `奇门八阵`,
-                },
+                        value: '官渡之战',
+                        label: `官渡之战`,
+                    }, {
+                        value: '奇门八阵',
+                        label: `奇门八阵`,
+                    },
                 ]}
                 onSelect={async (e) => {
-                    const url=`/map/${e}.csv`
+                    const url = `/map/${e}.csv`
                     const x = await axios.get(url)
                     const rows = []
                     let header = false
@@ -86,20 +114,60 @@ export class MapResource extends React.PureComponent<IProps, IState> {
                             header = true
                             continue
                         }
-                        rows.push(x1.split(","))
+                        rows.push(x1.split(",").concat(""))
                     }
                     this.setState({
                         rows
                     })
                 }}
             />
+            <Input style={{width: 100}} value={this.state.destination} onChange={(e) => {
+                const value = e.target.value
+                this.setState({
+                    destination: value
+                })
+            }}/>
+            <Button onClick={() => {
+                const {destination} = this.state
+                let values;
+                if (destination.includes(",")) {
+                    values = destination.split(",")
+                } else if (destination.includes("\t")) {
+                    values = destination.split("\t")
+                } else {
+                    values = destination.split(" ")
+                }
+                if (values.length > 1) {
+                    const point = {
+                        x: Number.parseInt(values[0]),
+                        y: Number.parseInt(values[1]),
+                    }
+                    const newRows = _.cloneDeep(this.state.rows)
+                    for (let i = 0; i < newRows.length; i++) {
+                        const cPoint = {
+                            x: Number.parseInt(newRows[i][5]),
+                            y: Number.parseInt(newRows[i][6]),
+                        }
+                        const d = this.calculateDistance(point, cPoint)
+                        newRows[i].push(d)
+                    }
+                    this.setState({
+                        rows: newRows
+                    })
+                }
+            }}>
+                计算距离
+            </Button>
             <Button onClick={this.exportToCsv}>
                 export csv
+            </Button>
+            <Button onClick={this.save}>
+                save
             </Button>
             <HotTable
                 ref={this.hotTableRef}
                 data={rows}
-                colHeaders={['id', '地图', '郡', '等级', '类型', 'x', 'y']}
+                colHeaders={['id', '地图', '郡', '等级', '类型', 'x', 'y', '拥有人', "计算距离"]}
                 language={zhCN.languageCode}
                 width={'auto'}
                 height={'85vh'}
